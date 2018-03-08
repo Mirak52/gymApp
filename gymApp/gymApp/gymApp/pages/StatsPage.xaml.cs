ce6b8fe30a567f11502fde6f2b899fc903d47173
bastlma14 <bastlma14@DST20707>
2018-03-08 14:08:36 +0100

    Statistiky, menu

--- gymApp/gymApp/gymApp/pages/StatsPage.xaml.cs
+++ gymApp/gymApp/gymApp/pages/StatsPage.xaml.cs
@@ -15,6 +15,43 @@ namespace gymApp.pages
         public StatsPage()
         {
             InitializeComponent();
+            SetPicker();
+        }
+
+        private void SetPicker()
+        {
+            var monkeyList = new List<string>();
+            monkeyList.Add("Sledované údaje");
+            monkeyList.Add("Maximálky");
+            Picker.ItemsSource = monkeyList;
+            
+        }
+
+        private void Picker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var pick = Picker.SelectedIndex;
+            if(pick == 0)
+            {
+                Stats.IsVisible = true;
+                BodyStats.IsVisible = true;
+                Records.IsVisible = false;
+            }
+            else if(pick == 1)
+            {
+                Stats.IsVisible = true;
+                BodyStats.IsVisible = false;
+                Records.IsVisible = true;
+            }
+        }
+
+        private void SendB_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendR_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
