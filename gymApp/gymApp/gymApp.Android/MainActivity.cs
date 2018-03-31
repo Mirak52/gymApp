dd37c811235555a66bb551fff2a8c5c7d7e0139c
Marek Bastl <bastlma14@sps-prosek.cz>
2018-03-31 22:28:33 +0200

    Zakázat rotaci

--- gymApp/gymApp/gymApp.Android/MainActivity.cs
+++ gymApp/gymApp/gymApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace gymApp.Droid
 {
-    [Activity(Label = "gymApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "gymApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Nosensor, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
